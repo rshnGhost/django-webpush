7afd6b4de742832e318b5266e93a83089b5b937c
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2022-12-15 20:06:22 +0530

    adding Subscription Info link in Push Information

--- webpush/admin.py
+++ webpush/admin.py
@@ -2,12 +2,14 @@ import json
 
 from django.contrib import admin
 
-from .models import PushInformation
+from .models import PushInformation, SubscriptionInfo
 from .utils import _send_notification
+from django.urls import reverse
+from django.utils.html import format_html
 
 
 class PushInfoAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "user", "subscription", "group")
+    list_display = ("__str__", "user", "subscription_link", "group_name")
     actions = ("send_test_message",)
 
     def send_test_message(self, request, queryset):
@@ -19,5 +21,18 @@ class PushInfoAdmin(admin.ModelAdmin):
             else:
                 self.message_user(request, "Deprecated subscription deleted")
 
+    def subscription_link(self, obj):
+        try:
+            href = reverse('admin:webpush_subscriptioninfo_change', kwargs={'object_id':obj.subscription.id})
+            link = format_html(f'<a href="{href}">{obj.subscription.browser}:{obj.subscription.auth}</a>')
+        except Exception as e:
+            print("error", e)
+            link = ''
+        return link
 
 admin.site.register(PushInformation, PushInfoAdmin)
+
+class SubscriptionInfoAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "browser", "auth", "p256dh")
+
+admin.site.register(SubscriptionInfo, SubscriptionInfoAdmin)
